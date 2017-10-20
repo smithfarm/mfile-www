15c57250ad6e482548a29e04a981870add59f670
Nathan Cutler <ncutler@suse.com>
2017-10-20 22:42:20 +0200

    0.164
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- lib/App/MFILE/WWW.pm
+++ lib/App/MFILE/WWW.pm
@@ -58,11 +58,11 @@ App::MFILE::WWW - Web UI development toolkit with prototype demo app
 
 =head1 VERSION
 
-Version 0.163
+Version 0.164
 
 =cut
 
-our $VERSION = '0.163';
+our $VERSION = '0.164';
 our @EXPORT_OK = ( '$VERSION' );
 
 
