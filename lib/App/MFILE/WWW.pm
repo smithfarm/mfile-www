368da2d6b4d13b36a1ab3183d0f6bcb163e6bf15
Nathan Cutler <ncutler@suse.com>
2017-11-01 07:40:58 +0100

    js: core: implement dcallback widget
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- lib/App/MFILE/WWW.pm
+++ lib/App/MFILE/WWW.pm
@@ -285,6 +285,16 @@ Like C<dform>, the primitive includes "miniMenu" functionality through which
 the user can potentially trigger actions that take the current object as input.
 
 
+=head2 dcallback
+
+The C<dcallback> primitive is useful for cases when none of the other primitives
+are appropriate for displaying a given type of object, and no interactivity is
+needed beyond that provided by miniMenu. The C<dcallback> primitive writes the
+target title and miniMenu to the screen, along with a "dcallback" div in
+between, which it populates by calling the callback function. Since the callback
+function part of the target definition, it can be app-specific.
+
+
 =head2 dform
 
 The C<dform> primitive is used to implement forms consisting of read-only
